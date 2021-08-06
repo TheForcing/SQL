b2e6f80b6607a4f23e3b3ea154bf91515e68efc3
박지훈 <qkrw6@naver.com>
2021-08-06 11:43:07 +0900

    ddl create, alter ,drop

--- 03-ddl-and-dcl.sql
+++ 03-ddl-and-dcl.sql
@@ -19,6 +19,7 @@
  --다시 생성
  create user c##bituser identified by bituser;
  
+ 
  --사용자 정보 확인
  -- user_: 현재 사용자 관련
  -- ALL_: 전체의 객체
@@ -55,3 +56,66 @@ REVOKE select on Hr.EMPLOYEES from c##bituser;
 Grant select ON HR.EMPLOYEES to c##bituser;
 -- 전체 권한 부여시
 -- GRANT ALL PRIVILEGES....
+
+create user c##bituser identified by bituser;
+
+-----------
+-- ddl
+-----------
+-- 이후 C##bituser로 진행
+
+--현재 내가 소유한 테이블 목록 확인
+select*from tab;
+-- 현재 나에게 주어진 role을 조회
+select*from User_role_privs;
+
+--create table: 테이블 생성
+create table book (
+     book_id number(5),
+     title varchar2(50),
+     author varchar2(10),
+     pub_data Date default sysdate
+);
+
+select*from tab;
+desc book;
+--서브쿼리를 이용한 테이블 생성
+--hr스키마의 employees 테이블의 일부 데이터를 추출, 새 테이블 생성
+
+select * from hr.employees;
+
+-- job_id 가 IT_관련 직원들만 뽑아내어 새 테이블 생성
+
+create Table It_emps as(
+ select*from hr.employees
+ where job_id like 'IT_$'
+ );
+ 
+ desc IT_EMPS;
+ 
+ -- author 테이블 추가
+ create table author (
+   author_id number(10),
+   author_name varchar(50) not null,
+   autohr_desc varchar2(500),
+   primary key (author_id) -- 테이블 제약
+   );
+   
+   desc author;
+   
+   ---- book 테이블의 author 컬럼 지우기
+   ---나중에 author 테이블과 fk 연결
+Desc book;
+alter table book drop column author;
+
+--- author 테이블 참조를 위한 컬럼 author_id 추가
+alter table book 
+add (author_id number(10));
+
+--book 테이블의 book_id도 number (10)으로 변경
+
+Alter table book modify (Book_id number(10));
+
+desc book;
+
+ 
